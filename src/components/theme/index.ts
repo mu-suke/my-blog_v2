47072b844c155a7cc1aa5859bd82d357131d687d
mu-suke <chelsea_08ym@yahoo.co.jp>
2022-04-18 00:53:55 +0900

    カラーテーマの追加

---
+++ src/components/theme/index.ts
@@ -0,0 +1,8 @@
+import { extendTheme } from '@chakra-ui/react'
+import { colors } from '@/components/theme/foundations/colors'
+
+const overrides = {
+  colors,
+}
+
+export default extendTheme(overrides)
