a45e2b14b75e48741795aa8fc07bb24d51ba963a
mu-suke <chelsea_08ym@yahoo.co.jp>
2022-04-18 01:59:07 +0900

    トップページにlayoutを適用

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,25 @@
 import { Box, Text } from '@chakra-ui/react'
 import PostBody from '@/components/blog/PostBody'
+import Layout from '@/components/layout'
 import { microCmsClient } from '@/libs/micro-cms-client'
 import { Blog } from '@/types/blog'
 import type { NextPage } from 'next'
 
 const Home: NextPage<{ blogList: Blog[] }> = ({ blogList }) => {
   return (
-    <Box>
-      {blogList.map((blog: Blog) => {
-        return (
-          <Box key={blog.id}>
-            <Text>{blog.title}</Text>
-            <PostBody body={blog.body} />
-            <Text>{blog.updatedAt}</Text>
-          </Box>
-        )
-      })}
-    </Box>
+    <Layout containerVariant={'default'}>
+      <Box>
+        {blogList.map((blog: Blog) => {
+          return (
+            <Box key={blog.id}>
+              <Text>{blog.title}</Text>
+              <PostBody body={blog.body} />
+              <Text>{blog.updatedAt}</Text>
+            </Box>
+          )
+        })}
+      </Box>
+    </Layout>
   )
 }
 
