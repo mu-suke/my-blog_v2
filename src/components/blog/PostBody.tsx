37f3353dede8e158bd9820045424e526274e7b10
mu-suke <chelsea_08ym@yahoo.co.jp>
2022-06-05 02:42:00 +0900

    VFCをFCに変更

--- src/components/blog/PostBody.tsx
+++ src/components/blog/PostBody.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@chakra-ui/react'
 import React from 'react'
 
-const PostBody: React.VFC<{ body: string }> = ({ body }) => {
+const PostBody: React.FC<{ body: string }> = ({ body }) => {
   return (
     <>
       <Box
