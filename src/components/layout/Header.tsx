8f94b7b9d311ef956aa76ee1662b8d247c537e81
mu-suke <chelsea_08ym@yahoo.co.jp>
2022-06-18 18:50:45 +0900

    共通ヘッダーの作成

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,29 +1,46 @@
-import { Container, Flex, Link, Image } from '@chakra-ui/react'
+import { Center, Container, Heading, Link, SimpleGrid } from '@chakra-ui/react'
 import React from 'react'
-import { PROJECT_NAME } from '@/constants'
+import NextLink from '@/components/elements/NextLink'
 
-const HEader: React.FC = () => {
+const Header: React.FC = () => {
+  // TODO(mu-suke): カテゴリやプロフィールができたらSimpleGridのcolumnsを変更する
   return (
     <>
-      <Container variant="lg" paddingY="0rem" height="4rem">
-        <Flex
-          paddingTop="6px"
-          alignItems={'center'}
-          justifyContent={'space-between'}
-        >
-          <Link href="/">
-            <Image
-              src="/images/vercel.svg"
-              alt={PROJECT_NAME}
-              objectFit={'cover'}
-              height={{ base: 6, md: 8 }}
-              width={'auto'}
-            />
-          </Link>
-        </Flex>
+      <Container variant="lg" paddingY="0rem">
+        <Center flexDirection={'column'} py={'1rem'}>
+          <NextLink href="/" passHref>
+            <Link color={'gray.800'} _hover={{ textDecoration: 'none' }}>
+              <Heading as="h1" size={'2xl'}>
+                むーすけのブログ
+              </Heading>
+            </Link>
+          </NextLink>
+          <SimpleGrid
+            columns={{ base: 2, md: 2 }}
+            spacing={{ base: 4, md: 8 }}
+            mt={'1rem'}
+          >
+            <Link
+              href={'https://github.com/mu-suke'}
+              isExternal
+              color={'gray.800'}
+              _hover={{ textDecoration: 'none', opacity: '0.5' }}
+            >
+              GitHub
+            </Link>
+            <Link
+              href={'https://twitter.com/chelsea08ym'}
+              isExternal
+              color={'gray.800'}
+              _hover={{ textDecoration: 'none', opacity: '0.5' }}
+            >
+              Twitter
+            </Link>
+          </SimpleGrid>
+        </Center>
       </Container>
     </>
   )
 }
 
-export default HEader
+export default Header
