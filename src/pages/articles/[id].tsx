ef47336aadd0f94d6b862d6e41505835dad55ac0
mu-suke <chelsea_08ym@yahoo.co.jp>
2022-06-20 22:50:50 +0900

    トップとブログ画面のコンテンツ高さを揃える

--- src/pages/articles/[id].tsx
+++ src/pages/articles/[id].tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { Box } from '@chakra-ui/react'
 import PostBody from '@/components/blog/PostBody'
 import PostHeader from '@/components/blog/PostHeader'
 import Layout from '@/components/layout'
@@ -8,9 +9,18 @@ import type { NextPage } from 'next'
 
 const Article: NextPage<{ article: Article }> = ({ article }) => {
   return (
-    <Layout containerVariant={'default'} isWhiteBg>
-      <PostHeader article={article} />
-      <PostBody body={article.body} />
+    <Layout containerVariant={'default'}>
+      <Box
+        backgroundColor={'white'}
+        px={{ base: '1rem', md: '1.5rem' }}
+        py={{
+          base: 0,
+          md: '1.5rem',
+        }}
+      >
+        <PostHeader article={article} />
+        <PostBody body={article.body} />
+      </Box>
     </Layout>
   )
 }
